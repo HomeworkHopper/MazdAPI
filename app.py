55dbfb68560953b2e7218ec6592ae6b808fc18d0
Shaun Thornton <homeworkhopper@gmail.com>
2023-03-30 20:06:56 -0400

    reimplemented hacky asyncio patch (unfortunately)

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import os
 
+import asyncio_patch
+
 from mapic import client
 from mapic import app
 
 if __name__ == "__main__":
-
     # generate a MapicConfig from config.yml
     config = client.MapicConfig.from_yaml(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.yml'))
 
     # create a new MapicClient
     app.config['mapic_client'] = client.MapicClient(config)
+
+    # patch asyncio bug (windows only)
+    asyncio_patch.patch()
 
     # run the flask application
     app.run(host="0.0.0.0", debug=False, use_reloader=False)
---
+++ asyncio_patch.py
@@ -0,0 +1,23 @@
+import platform
+
+from functools import wraps
+
+
+# see https://github.com/aio-libs/aiohttp/issues/4324#issuecomment-733884349
+
+def patch():
+    if platform.system() == 'Windows':
+        from asyncio.proactor_events import _ProactorBasePipeTransport
+
+        def silence_event_loop_closed(func):
+            @wraps(func)
+            def wrapper(self, *args, **kwargs):
+                try:
+                    return func(self, *args, **kwargs)
+                except RuntimeError as e:
+                    if str(e) != 'Event loop is closed':
+                        raise
+
+            return wrapper
+
+        _ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
